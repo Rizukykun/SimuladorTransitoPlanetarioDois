dd2b59cc9f6bf18bcf86c471a99af505a574c5b4
Guilherme Barros <guilhermebarrosrl@gmail.com>
2023-05-02 02:11:18 -0300

    Alteração para otimizar a utilização de código e remoção do FFMPEG

--- webapp/Models/Main.py
+++ webapp/Models/Main.py
@@ -1,6 +1,5 @@
 from __future__ import unicode_literals
 import matplotlib
-from sqlalchemy import false
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 from matplotlib import gridspec
@@ -8,7 +7,7 @@ from webapp.SMOOTH import blur_image
 import numpy as np
 import os
 import glob
-from webapp.automold import add_fog
+import cv2
 
 class Main (object):
 
@@ -20,25 +19,40 @@ class Main (object):
 			os.remove("webapp/static/video/test.webm")
 		except OSError:
 			pass
-		os.system("ffmpeg -r 10 -start_number 40 -i ./webapp/output/frames/img_%d.png -b:v 1000k ./webapp/static/video/test.webm")
+		imgList = []
+		for filename in glob.glob('webapp/output/frames/*.png'):
+			img = cv2.imread(filename)
+			height, width, layers = img.shape
+			size = (width,height)
+			imgList.append(img)
+
+		out = cv2.VideoWriter('webapp/static/video/test.webm', cv2.VideoWriter_fourcc('V','P','8','0'), 15, size)
+		for i in range(len(imgList)):
+			out.write(imgList[i])
+		out.release()
 		
 	def plotImgs(self, planet, star, moons, maps, noise):
 	
+		#Parâmetros de plotagem
 		plt.rcParams['xtick.minor.visible'] = True
 		plt.rcParams['ytick.minor.visible'] = True
 		plt.rcParams['legend.borderaxespad'] = .3
 		plt.rcParams['legend.handlelength'] = 1.
+		color = '.9'
+		g = 1
 		
-		sMa = planet.orbit.semiaxis #semi-major axis of planetary orbit (in Rstar)
-		Rs_pix = star.radius #stellar radius (in ~pixel?)
-		Rplan = planet.radius #planetary radius (in Rstar) (1 Rjup = 6.9911e4 km = 0.100447 Rsun)
+		Rs_pix = star.radius #Raio estimado da estrela em pixels
+		Rplan = planet.radius #Raio do planeta em RSol (1 Rjup = 6.9911e4 km = 0.100447 Rsun)
 		ImgStar = star.returnimgStar()
-		if (float(noise) > 0):
-			ImgStar = add_fog(ImgStar, fog_coeff= float(noise))
+		ImgStarSoma = ImgStar.sum()
+		
+		#A maneira de gerar o ruído será diferente. Não precisaremos disso agora. Estará comentado até implementarmos o noise
+		# if (float(noise) > 0):
+		# 	ImgStar = add_fog(ImgStar, fog_coeff= float(noise))
 
 		Ny, Nx = ImgStar.shape
 
-		Rpl_pix = Rplan * Rs_pix
+		Rpl_pix = Rplan * Rs_pix #Raio do planeta em pixel
 
 		coordp = planet.planetOrbit(Rs_pix, ImgStar)
 		xp = coordp[0]
@@ -46,38 +60,18 @@ class Main (object):
 
 		tlstep = planet.tlstep(Rs_pix, ImgStar)
 		tstep = tlstep[0]
-		Lstep = tlstep[1]
+		lc = tlstep[1]
 		
-		
+
 		#VARIAVEIS DE MATRIZ
 		kk = np.arange(Ny * Nx) 
 
 		q, = np.where((xp+(Nx/2) + 98 > 0) & (xp+(Nx/2) - 98 < Nx))
 		i = int(np.mean(q))
-		x0 = xp[i] + Nx/2
-		y0 = yp[i] + Ny/2
-		planet = np.zeros(Ny * Nx) + 1
 		
-		# Planet:
-		ii, = np.where((kk/Nx-y0)**2+(kk-Nx*np.int64(1.*kk/Nx)-x0)**2 < Rpl_pix**2)
-		planet[ii] = 0.
-
-		
-		# Moons:
-		if len(moons) > 0:
-			for x in range(0, len(moons)):
-				
-				ll, = np.where((kk/Nx-(y0-moons[x].moonOrbit(Rs_pix, Rpl_pix, tstep)[1][i]))**2+(kk-Nx*np.int64(kk/Nx)-(x0-moons[x].moonOrbit(Rs_pix, Rpl_pix, tstep)[0][i]))**2 < moons[x].rMoon(Rs_pix)**2)
-				planet[ll]=0.
-		
-		planet = planet.reshape([Ny, Nx])
-				
-		#Lmin = np.sum(ImgStar*planet) / ImgStar.sum()
-		Lmin = 0.999 #verificar o código comentado acima posteriormente
-
-		
-		color = '.9'
-		g = 1
+		#os valores estão invertidos para que a verificação dos limites reais seja realizada no laço de plotagem		
+		Lmin = 1 #Limite mínimo do Gráfico
+		Lmax = 0 #Limite máximo do Gráfico
 
 		## @@@ Guilherme Barros - Primeiro incremento de teste - Deleção de imagens da pasta
 		files = glob.glob('webapp/output/frames/*')
@@ -89,38 +83,39 @@ class Main (object):
 
 		differenceRate = 0
 		rateCalculation = False
-		Lmax = 0
 
 		#print 'Loop start. '
 		for i in q:
+			#Inicializa a geração da sombra na frente da estrela
 			planet = np.zeros(Ny * Nx) + 1
 
 			x0 = xp[i] + Nx/2
 			y0 = yp[i] + Ny/2
 
-			# Planet:
+			# Planeta:
 			ii, = np.where((kk/Nx-y0)**2+(kk-Nx*np.int64(1.*kk/Nx)-x0)**2 < Rpl_pix**2)
 			planet[ii] = 0.
 
-			# Moon:
+			# Lua(s):
 			if len(moons) > 0:
 				for x in range(0, len(moons)):
 					ll, = np.where((kk/Nx-(y0-moons[x].moonOrbit(Rs_pix, Rpl_pix, tstep)[1][i]))**2+(kk-Nx*np.int64(kk/Nx)-(x0-moons[x].moonOrbit(Rs_pix, Rpl_pix, tstep)[0][i]))**2 < moons[x].rMoon(Rs_pix)**2)
 					planet[ll]=0.
+			
 			planet = planet.reshape([Ny, Nx])
 
-			# Smooth image
+			# Smooth image (talvez não precise disso)
 			img = blur_image(ImgStar*planet, 5)
 
-			Lstep[i] = np.sum(img)
-			#if L!=0: Lstep[i] = np.sum(img)* np.random.np.random.normal(1., 3e-4)
-			#else:    Lstep[i] = np.sum(img)
-			lc = Lstep / ImgStar.sum()
-			#lc = lc + (1 - np.nanmax(lc))
+			#Realiza a soma de todos os pontos da imagem atual
+			#Lstep[i] = np.sum(img)
 
-			if (not rateCalculation):
-				differenceRate = 1 - lc[i]
-				rateCalculation = True			
+			lc[i] = np.sum(img) / ImgStarSoma
+
+			#Verificar depois
+			# if (not rateCalculation):
+			# 	differenceRate = 1 - lc[i]
+			# 	rateCalculation = True			
 
 			#lc+= differenceRate
 			# #estudar difference rate com o fábio	
@@ -132,13 +127,13 @@ class Main (object):
 			ax = plt.subplot(gs[:3, :])
 			ax.imshow(img, cmap=maps, interpolation='gaussian', origin='lower')
 			ax = plt.subplot(gs[3:, :])
-			ax.plot(tstep[:i], lc[:i], '.-', lw=.8, ms=3, c='#ef9f4f')
+			ax.plot(tstep[q[0]:i], lc[q[0]:i], '.-', lw=.8, ms=3, c='#ef9f4f')
 			ax.set_xlim(tstep[0], tstep[-1])#*1.1)
 			if (Lmax < lc[i]):
 				Lmax = lc[i]
 			if (Lmin > lc[i]):
 				Lmin = lc[i]
-			ax.set_ylim(Lmin*.999, Lmax*.001)
+			ax.set_ylim(Lmin*.999, Lmax + 0.001)
 			ax.set_xlabel('time (h)', fontweight='bold')
 			ax.set_ylabel(r'flux (L$\star$)', fontweight='bold')
 			ax.grid(which='major', c=color, alpha=.6, lw=.6)
@@ -150,7 +145,7 @@ class Main (object):
 			ax.yaxis.label.set_color(color)
 			ax.tick_params(axis='both', which='both', colors=color)
 			fig.tight_layout()			
-			fig.savefig('webapp/output/frames/img_' + str(i) + '.png', dpi=128, facecolor='k')
+			fig.savefig('webapp/output/frames/img_' + str(f"{i:03}") + '.png', dpi=128, facecolor='k')
 			plt.close('all')
 			g += 1
 			# =====================================================================
